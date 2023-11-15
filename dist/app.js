6cb9299f4a9b68822e443775ff61ceacd7820732
taha tamizi <t-tamizi@IMI.COM>
2023-11-15 16:59:51 +0330

    made changes

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
   if (window.innerWidth > 1023) {
+    var navbar = document.getElementById('navbar')
+
     document.addEventListener('scroll', function () {
       var navbar = document.getElementById('navbar')
+
       if (window.scrollY > 50) {
         navbar.classList.add('scrolled')
+          navbar.classList.add('hide-elements')
       } else {
         navbar.classList.remove('scrolled')
+
         navbar.classList.remove('hide-elements')
       }
     })
