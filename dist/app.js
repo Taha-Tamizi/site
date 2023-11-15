ec7983752c1a1b58bcb553cc30889e61fcc5f464
taha-tamizi <taha1381.tt1@gmail.com>
2023-11-15 22:01:15 +0330

    made changes

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
   if (window.innerWidth > 1023) {
-    var navbar = document.getElementById('navbar')
-
     document.addEventListener('scroll', function () {
       var navbar = document.getElementById('navbar')
 
       if (window.scrollY > 50) {
         navbar.classList.add('scrolled')
-          navbar.classList.add('hide-elements')
+        navbar.classList.add('hide-elements')
       } else {
         navbar.classList.remove('scrolled')
 
@@ -21,17 +19,16 @@ document.addEventListener('DOMContentLoaded', function () {
   var video = document.getElementById('myVideo')
   var playButton = document.getElementById('playButton')
 
-  // Hide the default controls initially
   video.removeAttribute('controls')
 
   playButton.addEventListener('click', function () {
     if (video.paused) {
       video.play()
-      playButton.style.display = 'none' // Hide the play button when video is playing
-      video.setAttribute('controls', 'controls') // Show the default controls when video is playing
+      playButton.style.display = 'none'
+      video.setAttribute('controls', 'controls')
     } else {
       video.pause()
-      playButton.style.display = 'block' // Show the play button when video is paused
+      playButton.style.display = 'block'
     }
   })
 })
