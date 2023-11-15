e1c16be02620ab16de82796507b5c0558441fe5e
taha tamizi <t-tamizi@IMI.COM>
2023-11-15 15:08:41 +0330

    made changes

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,13 @@
-// Initialization for ES Users
-import { Carousel, initTE } from 'tw-elements'
-
-initTE({ Carousel })
-
+document.addEventListener('DOMContentLoaded', function () {
+  if (window.innerWidth > 1023) {
+    document.addEventListener('scroll', function () {
+      var navbar = document.getElementById('navbar')
+      if (window.scrollY > 50) {
+        navbar.classList.add('scrolled')
+      } else {
+        navbar.classList.remove('scrolled')
+        navbar.classList.remove('hide-elements')
+      }
+    })
+  }
+})
