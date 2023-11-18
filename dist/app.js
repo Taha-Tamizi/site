aa582c6312b8f625e0a738a46fe8c72581dbd762
taha-tamizi <taha1381.tt1@gmail.com>
2023-11-18 12:36:26 +0330

    navabr

--- dist/app.js
+++ dist/app.js
@@ -4,9 +4,9 @@ document.addEventListener('DOMContentLoaded', function () {
       var navbar = document.getElementById('navbar')
 
       if (window.scrollY > 5) {
-        navbar.classList.add('scrolled')
+        navbar.classList.add('scrolled', 'fixed') 
       } else {
-        navbar.classList.remove('scrolled')
+        navbar.classList.remove('scrolled', 'fixed')
       }
       if (window.scrollY > 0) {
         navbar.classList.add('hide-elements')
